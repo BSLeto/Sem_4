32dee87d3d73d2f1b0088345fc84b9bf0afb93c3
Leto <mikhaylichenko2000@bk.ru>
2022-08-14 16:12:24 +0300

    Выполнили задачу №29

---
+++ HW_29/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+// 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] GetBinaryArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+
+    }
+    return result;
+}
+Console.Write($"Введите необходимую длинну массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите минимальное значение в массиве: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите максимальное значение в массиве: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] testArray = GetBinaryArray(number, min, max);
+Console.WriteLine($"Результат: [{String.Join(", ", testArray)}]");
+
+
