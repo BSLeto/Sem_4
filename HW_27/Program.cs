c58365068a4c939dea649101e9182863e8a3cedb
Leto <mikhaylichenko2000@bk.ru>
2022-08-14 16:07:24 +0300

    Выполнизи задачу № 27

---
+++ HW_27/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+//===============================
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//===============================
+
+Console.Write($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int sum = 0;
+while (number > 0)
+{
+    sum += number%10;
+    number /= 10;
+
+    i++;
+}
+Console.Write($"{sum} ");
